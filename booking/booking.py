3f9c9299490e38d4554a1637b0e94c9a6dab906c
faris771 <farisabufarha33@gmail.com>
2022-09-19 00:17:19 +0300

    runs until search button

--- booking/booking.py
+++ booking/booking.py
@@ -19,17 +19,65 @@ class Booking(
         super().__init__(
             service=service)  # this shit is for the Daddy class Chrome # THIS TOOK ME FKING LONG TOME TO ACKNOWLEDGE GET BACK TO SEC 3 TO UNDERSTAND
         # self.maximize_window()
-        self.implicitly_wait(15)
+        self.implicitly_wait(30)
 
         # the idea is that we pass the service thing to the parent class(chrome) for each object
 
     def land_first_page(self):
         self.get(const.BASE_URL)
 
-    def change_currency(self, currency=None):
-        currency_button = self.find_element(By.XPATH, '//button[@data-tooltip-text="Choose your currency"]')
+    def change_currency(self, currency="EUR"):
+        self.implicitly_wait(30)
 
+        currency_button = self.find_element(By.XPATH, '//button[@data-tooltip-text="Choose your currency"]')
         currency_button.click()
+        select_currency_btn = self.find_element(By.CSS_SELECTOR,
+                                                f'a[data-modal-header-async-url-param*="selected_currency={currency}"]')
+        # this * added cuz the atrb we chose has substring and we chose this part notice the ;& stuff
+
+        select_currency_btn.click()
+
+    def select_place(self, place='Ramallah'):
+        self.implicitly_wait(30)
+
+        place_field = self.find_element(By.ID, 'ss')
+        place_field.clear()
+        place_field.send_keys(f'{place}')
+        first_in_list = self.find_element(By.CSS_SELECTOR, 'li[data-i="0"]')
+        first_in_list.click()
+
+    def select_date(self, check_in="2022-09-22", check_out="2022-10-11"):
+        self.implicitly_wait(30)
+        in_date = self.find_element(By.CSS_SELECTOR, f'td[data-date="{check_in}"]')
+        in_date.click()
+        out_date = self.find_element(By.CSS_SELECTOR, f'td[data-date="{check_out}"]')
+        out_date.click()
+
+    def select_members(self, target_num=7):
+        self.implicitly_wait(30)
+
+        members_button = self.find_element(By.ID, 'xp__guests__toggle')
+        members_button.click()
+        current_num = self.find_element(By.ID, 'group_adults')
+
+        dec_button = self.find_element(By.CSS_SELECTOR, 'button[aria-label="Decrease number of Adults"]')
+        inc_button = self.find_element(By.CSS_SELECTOR, 'button[aria-label="Increase number of Adults"]')
+
+        while True:
+            current_num2 = current_num.get_attribute('value')  # will return current num
+
+            if int(current_num2) == 1:
+                break
+
+            dec_button.click()
+
+        for i in range(target_num - 1):
+            inc_button.click()
+
+    def click_search(self):
+
+        search_button = self.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
+        search_button.click()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.tear_down:
--- run.py
+++ run.py
@@ -2,8 +2,19 @@ from booking.booking import Booking
 from booking.constants import *
 
 
-with Booking(tear_down=False) as bot:
-    bot.land_first_page()
-    bot.change_currency()
+def main():
+
+    with Booking(tear_down=False) as bot:
+
+        bot.land_first_page()
+        bot.change_currency(currency="ILS")
+        bot.select_place() #by default Ramallah
+        bot.select_date()
+        bot.select_members(target_num=1)
+        bot.click_search()
 
 
+
+
+if __name__ == '__main__':
+    main()
